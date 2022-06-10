7c6a3023f84fa5fe4b91c8dc9a1e30ce2b90d3df
MR X PHANTOM <99727604+MR-X-PHANTOM@users.noreply.github.com>
2022-06-11 00:16:48 +0530

    Update start.py

--- SemxXSpam/plugins/start.py
+++ SemxXSpam/plugins/start.py
@@ -4,11 +4,11 @@ from .. import MK1, MK2, MK3, MK4, MK5, MK6, MK7, MK8, MK9, MK10, OWNER_ID
 from SemxXSpam.plugins.help import *
 
 
-RYAN_IMG = "https://te.legra.ph/file/91d82bf74b7a75468f5ef.jpg"
+RYAN_IMG = "https://te.legra.ph/file/d79e4d7694985a5070db6.jpg"
 
 Ryan_Button = [
         [
-        Button.url("✨ sᴜᴘᴘᴏʀᴛ ✨", "https://t.me/TheMKHackerX131")
+        Button.url("✨ sᴜᴘᴘᴏʀᴛ ✨", "https://t.me/XX_ll_SP4M_GOD_lI_XX")
         ],
         [
         Button.inline("⚡ ᴄᴏᴍᴍᴀɴᴅs ⚡", data="help_back")
@@ -17,11 +17,11 @@ Ryan_Button = [
                
 RyanX_Button = [
         [
-        Button.url("✨ ᴄʜᴀɴɴᴇʟ ✨", "https://t.me/TheMKHackerX131"),
-        Button.url("✨ sᴜᴘᴘᴏʀᴛ ✨", "https://t.me/HACKERxSPAM")
+        Button.url("✨ ᴄʜᴀɴɴᴇʟ ✨", "https://t.me/XX_ll_SP4M_GOD_lI_XX"),
+        Button.url("✨ sᴜᴘᴘᴏʀᴛ ✨", "https://t.me/xx_ll_SP4M_GOD_ll_xx")
         ],
         [
-        Button.url("🔥 ʀᴇᴘᴏ 🔥", "https://github.com/ERR0rMK/SEMXxBOTFATHER")
+        Button.url("🔥 ʀᴇᴘᴏ 🔥", "https://github.com/MR-X-PHANTOM/SEMXxBOTFATHER")
         ]
         ]
         
@@ -49,7 +49,7 @@ async def start(event):
        TheRyan = event.chat_id
        firstname = replied_user.user.first_name
        userid = replied_user.user.id
-       ownermsg = f"**Hello Boss !!, It's Me {bot_name}, Your Spam Bot !! \n\n Click Below Buttons For Help. 🌚**"
+       ownermsg = f"**Hello Boss !!, It's Me {bot_name}, SPAM X GOD !! \n\n Click Below Buttons For Help. 🌚**"
        usermsg = f"**Hello !! [{firstname}](tg://user?id={userid})\nNice To Meet You, Well I Am [{bot_name}](tg://user?id={bot_id}), A Powerfull Spam Bot.** \n\n**If You Want Your Own Spam Bots You Can Deploy From The Button Given Below.** \n\n**Powered By : [𝙎𝙚𝙢𝙭𝙓𝙎𝙥𝙖𝙢](https://t.me/rudra_hun_vaii)**"
        if event.sender_id == OWNER_ID:
             await event.client.send_file(TheRyan,
