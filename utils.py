4151132d4941de23098b74b3377d080c990f298b
MR X PHANTOM <99727604+MR-X-PHANTOM@users.noreply.github.com>
2022-06-24 16:16:21 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -12,7 +12,7 @@ def load_plugins(plugin_name):
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
     sys.modules["SemxXSpam.plugins." + plugin_name] = load
-    print("Semx X has Imported " + plugin_name)
+    print("SP4M X GOD has Imported " + plugin_name)
 
 async def edit_or_reply(event, text):
     if event.sender_id in SUDO_USERS:
